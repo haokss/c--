59dddbcbf323ee6b41441bdd62afe84e9ef49a9b
haoks <2920611818@qq.com>
2023-09-29 00:04:50 +0800

    study1

--- Template_specializtion/main.cpp
+++ Template_specializtion/main.cpp
@@ -14,6 +14,34 @@ int compare(const std::string &v1, const std::string &v2){
     std::cout << "特化版本" << std::endl;
     return strcmp(v1.c_str(), v2.c_str());
 }
+template <class T1, class T2>
+class Data
+{
+public:
+    Data() { cout << "Data<T1, T2>" << endl; }
+
+private:
+    T1 _d1;
+    T2 _d2;
+};
+
+//全特化<int,char>
+template <>
+class Data<int, char>
+{
+public:
+    Data() { std::cout << "Data<int, char>" << std::endl; }
+
+private:
+    int _d1;
+    char _d2;
+};
+
+void TestVector()
+{
+    Data<int, int> d1;
+    Data<int, char> d2;
+}
 
 int main(int argc, char const *argv[])
 {
